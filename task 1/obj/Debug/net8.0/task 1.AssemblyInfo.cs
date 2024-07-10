5e7cd644757335508ce6105aaa4a0636d6708c45
Yegor <abramov.yegor.00@mail.ru>
2024-07-10 15:08:19 +0300

    task 3

--- task 1/obj/Debug/net8.0/task 1.AssemblyInfo.cs
+++ task 1/obj/Debug/net8.0/task 1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("task 1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f7d693f851acb9ae5011e16545dc20b9b05736c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c834da9e486e3566b974a303fb534e8f24f094")]
 [assembly: System.Reflection.AssemblyProductAttribute("task 1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("task 1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- task 3/Program.cs
+++ task 3/Program.cs
@@ -1,2 +1,43 @@
 ﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца.
 // Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize, 0, 10);
+    PrintArray(array);
+    Console.WriteLine();
+    ReverseArray(array, arraySize);
+}
+
+int ReverseArray(int[] array, int arraySize)
+{
+    int temp = arraySize;
+    if (temp == 0)
+        return array[temp];
+    Console.Write($"{array[temp - 1]}" + " ");
+    return ReverseArray(array, temp - 1);
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    Console.WriteLine("[" + string.Join(", ", arrayForPrint) + "]");
+}
+
+int[] GenerateArray(int size, int minValue, int maxValue)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+
+    for (int i = 0; i < tempArray.Length; i++)
+        tempArray[i] = rand.Next(minValue, maxValue);
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
